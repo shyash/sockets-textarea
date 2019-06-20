73bc1a733dddc2dd7699336da7f6e6c71da218bd
shyash <shyash222@gmail.com>
2019-06-20 11:49:36 +0530

    modified

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const PORT = process.env.PORT || 8800;
 const IP = process.env.IP || '0.0.0.0';
-const express = require("express");
+const express = require('express');
 const app = express();
+const socket = require('socket.io')
+const server = app.listen(PORT, IP, () => {
+  console.log('Server started!')
+})
+const io = socket(server)
+io.sockets.on('connection', newConnection)
+
+function newConnection(socket) {
+	console.log('Connection : ' + socket.id)
+	socket.on('textareaChanged', changeTextarea)
+	function changeTextarea(data) {
+		socket.broadcast.emit('textareaChanged', data)
+		console.log(data)
+	}
+}
+
 app.get("/", function (req,res) {
-	res.sendFile('index.html',{root: __dirname + '/views'})
+	res.sendFile('index.html', {root: __dirname + '/views'})
 })
 app.get("*", function (req, res) {
   res.send("404 URL NOT FOUND");
-});
-app.listen(PORT, IP, () => {
-  console.log('Server started!')
-})
+}); 
